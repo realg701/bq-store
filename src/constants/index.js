d4250f407590e539dcc4a3b297c269187070c895
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-25 05:38:28 +0500

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const checkOutTextField = [
+  { value: order.name, name: "name", label: "Name" },
+  { value: order.phoneNumber, name: "phoneNumber", label: "Phone no." },
+  { value: order.address, name: "address", label: "Address" },
+  { value: order.email, name: "email", label: "Email" },
+];
