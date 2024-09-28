f0dc1f9826ffbc6a98c611e74a7311c480af098d
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-28 07:15:59 +0500

    Update index.jsx

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import CartContext from "../../Context/CartContext";
 import "./cart.css";
@@ -7,117 +7,107 @@ import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
 import HomeIcon from "@mui/icons-material/Home";
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
 import LoginIcon from "@mui/icons-material/Login";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 export default function Cart() {
   const navigate = useNavigate();
 
   const cartContext = useContext(CartContext);
-  const { cartItems, removeFromCart } = cartContext;
+  const { cartItems, setCartItems, removeFromCart } = cartContext;
   const user = JSON.parse(localStorage.getItem("user"));
-  console.log(cartItems);
+  const [number, setNumber] = useState(1); //number of item
+
+  const updateQuantity = (id, value) => {
+    cartItems.map((item) => item.id === id) &&
+      setNumber((prevState) => prevState + value);
+  };
+
+  // this is part of other files return statement
+
   return (
-    <>
-      <div className="container">
-        {cartItems.length ? (
-          cartItems.map((item, itemIndex) => (
-            <div className="cart-flex" key={itemIndex}>
-              <div className="cart-container">
-                <Link to={`/products/${item._id}`}>
-                  <div className="cart-content">
-                    <img className="cart-image" src={item.image} alt="" />
-                    <div className="cart-title">
-                      <p>{item.title}</p>
-                      <span className="saperater-min">|</span>
-                      <p>
-                        Qty. {(item.qty, 3)} | Rs. {item.price}
-                      </p>
-                    </div>
-                  </div>
-                </Link>
-                <div className="cart-btn">
-                  <div>
-                    <span className="saperater-min">
-                      <Button
-                        variant="contained"
-                        color="error"
-                        startIcon={<RemoveShoppingCartIcon />}
-                        className="remove-from-cart remove-min"
-                        onClick={() => removeFromCart(item.title)}
-                      >
-                        Remove
-                      </Button>
-                    </span>
-                    <span className="saperater-max">
-                      <Button
-                        variant="contained"
-                        color="error"
-                        className="remove-from-cart remove-max"
-                        onClick={() => removeFromCart(item.title)}
-                      >
-                        <RemoveShoppingCartIcon />
-                      </Button>
-                    </span>
-                  </div>
+    <div className="cart">
+      <h1>Cart Items</h1>
+      {cartItems.length ? (
+        <div className="cart-container">
+          {cartItems.map((item, itemIndex) => (
+            <div className="cart-card" key={itemIndex}>
+              <Link to={`/products/${item._id}`} className="cart-content">
+                <img src={item.image} alt={item.title} />
+                <div className="cart-title">
+                  <p>{item.title}</p>
+                  <p>Category: {item.category}</p>
                 </div>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="cart-flex">
-            <div className="cart-non-container">
-              <div className="cart-non-title">
-                <p>No items in Cart</p>
-              </div>
-              <div className="cart-non-title home-btn">
+              </Link>
+              <div className="cart-btns">
+                <div className="quantity-btns">
+                  <Button
+                    variant="text"
+                    color="warning"
+                    onClick={(item) => item.qty - 1}
+                  >
+                    <RemoveCircleIcon />
+                  </Button>
+                  <p>
+                    Qty. {item.qty} | Rs. {item.price}
+                  </p>
+                  <Button
+                    variant="text"
+                    color="success"
+                    onClick={() => updateQuantity(item._id, 1)}
+                  >
+                    <AddCircleIcon />
+                  </Button>
+                </div>
                 <Button
-                  onClick={() => navigate("/")}
+                  className="remove-item"
                   variant="contained"
-                  startIcon={<HomeIcon />}
-                  color="warning"
-                  size="large"
+                  color="error"
+                  onClick={() => removeFromCart(item.title)}
                 >
-                  Back to Home
+                  <RemoveShoppingCartIcon />
                 </Button>
               </div>
             </div>
-          </div>
-        )}
-        {cartItems.length ? (
-          <div className="cart-flex">
-            <div className="cart-non-container">
-              <div className="cart-non-title home-btn">
-                {user ? (
-                  <>
-                    <Button
-                      onClick={() => navigate("/checkout")}
-                      variant="contained"
-                      startIcon={<ShoppingCartCheckoutIcon />}
-                      color="warning"
-                      size="large"
-                    >
-                      Checkout
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    <Button
-                      onClick={() => navigate("/login")}
-                      variant="contained"
-                      startIcon={<LoginIcon />}
-                      color="warning"
-                      size="large"
-                    >
-                      Please Login
-                    </Button>
-                  </>
-                )}
-              </div>
-            </div>
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
-    </>
+          ))}
+          {user ? (
+            <Button
+              className="checkout"
+              onClick={() => navigate("/checkout")}
+              variant="contained"
+              startIcon={<ShoppingCartCheckoutIcon />}
+              color="warning"
+              size="large"
+            >
+              Checkout
+            </Button>
+          ) : (
+            <Button
+              className="checkout"
+              onClick={() => navigate("/login")}
+              variant="contained"
+              startIcon={<LoginIcon />}
+              color="warning"
+              size="large"
+            >
+              Please Login
+            </Button>
+          )}
+        </div>
+      ) : (
+        <div className="cart-container">
+          <p>No items in Cart</p>
+          <Button
+            onClick={() => navigate("/")}
+            variant="contained"
+            startIcon={<HomeIcon />}
+            color="warning"
+            size="large"
+          >
+            Back to Home
+          </Button>
+        </div>
+      )}
+    </div>
   );
 }
