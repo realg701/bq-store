5e7669e85be61b1115ff43206bd1b3303fd5b848
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-29 00:46:19 +0500

    Update CartContext.jsx

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -15,13 +15,33 @@ export const CartContainer = ({ children }) => {
     const items = [...cartItems, newItem];
     setCartItems(items);
   };
+
   const removeFromCart = (name) => {
     const filteredItems = cartItems.filter((item) => item.title !== name);
     setCartItems(filteredItems);
   };
 
+  const handleQuantity = (p, value) => {
+    let product = cartItems.find((pr) => pr.title === p.title);
+    product.quantity += value;
+    if (product.quantity < 1) product.quantity = 1;
+    const updatedProducts = cartItems.map((p) => {
+      if (product.title === p.title) return product;
+      return p;
+    });
+    setCartItems(updatedProducts);
+  };
+
   return (
-    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{
+        cartItems,
+        setCartItems,
+        handleQuantity,
+        addToCart,
+        removeFromCart,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
