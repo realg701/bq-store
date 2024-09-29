40e2ce7ebc47d780ed81612927acdc7df7f9656e
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-30 04:09:19 +0500

    Update index.jsx

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 import { useNavigate } from "react-router";
-import "./checkout.css";
+import { Link } from "react-router-dom";
+import { BE_URL } from "../../constants/url";
+import CartContext from "../../Context/CartContext";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import TextField from "@mui/material/TextField";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
-import { Container } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
-import { BE_URL } from "../../constants/url";
-import CartContext from "../../Context/CartContext";
-import { Link } from "react-router-dom";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
 import HomeIcon from "@mui/icons-material/Home";
 import LoginIcon from "@mui/icons-material/Login";
+import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
+import "./checkout.css";
+// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
 
 const CheckOut = () => {
   const navigate = useNavigate();
   const cartContext = React.useContext(CartContext);
-  const { cartItems } = cartContext;
+  const { cartItems, setCartItems, handleQuantity, removeFromCart } =
+    cartContext;
   // const token = JSON.parse(localStorage.getItem("token"));
   const user = JSON.parse(localStorage.getItem("user"));
   const [open, setOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [message, setMessage] = React.useState("No items in Cart");
+  const [order, setOrder] = React.useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+  });
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -28,12 +42,25 @@ const CheckOut = () => {
     }
     setOpen(false);
   };
+
   const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
   });
 
-  const [order, setOrder] = React.useState({});
-  const [loading, setLoading] = React.useState(false);
+  const checkOutTextField = [
+    { value: user?.userName || order.name, name: "name", label: "Name" },
+    {
+      value: user?.phoneNumber || order.phoneNumber,
+      name: "phoneNumber",
+      label: "Phone no.",
+    },
+    {
+      value: user?.address || order.address,
+      name: "address",
+      label: "Address",
+    },
+    { value: user?.email || order.email, name: "email", label: "Email" },
+  ];
 
   const handleChange = (e) => {
     const { value, name } = e.target;
@@ -42,28 +69,46 @@ const CheckOut = () => {
     });
   };
 
+  const productData = cartItems.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      image: item.image,
+      price: item.price,
+      category: item.category,
+      seller: item.seller,
+      quantity: item.quantity,
+    };
+  });
+
   const handleSubmit = async () => {
+    if (!order.name || !order.phoneNumber || !order.address || !order.email)
+      return setOpen(true);
     setLoading(true);
     fetch(`${BE_URL}/orders/order`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        products: cartItems,
         name: order.name,
         phoneNumber: order.phoneNumber,
         address: order.address,
         email: order.email,
+        products: productData,
       }),
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log("Apply:", data);
+        console.log("CheckOut:", data);
         if (data.status === "success") {
-          console.log("Registered successfully");
+          setCartItems([]);
+          setMessage("Order Placed Successfully");
           setLoading(false);
         }
         if (data.status === "error") {
-          console.log("Try a different username or email");
+          setLoading(false);
+        }
+        if (data.status === "empty") {
+          setMessage("Your Cart Is Empty");
           setLoading(false);
         }
       })
@@ -72,152 +117,175 @@ const CheckOut = () => {
         setLoading(false);
       });
   };
+
+  const goToBottom = () => {
+    window.scrollTo({
+      top: window.screen.height,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
-      <div className="container">
-        <div className="add-flex">
-          <div className="add-container">
-            {loading ? (
-              <>
-                <CircularProgress />
-              </>
-            ) : (
-              <>
-                {user ? (
-                  <Container className="input-container">
-                    <h3>Order Product</h3>
-                    <TextField
-                      required
-                      fullWidth
-                      value={order.name}
-                      onChange={handleChange}
-                      name="name"
-                      label="Name"
-                      variant="outlined"
-                      className="text-field"
-                    />
-                    <TextField
-                      required
-                      fullWidth
-                      value={order.phoneNumber}
-                      onChange={handleChange}
-                      name="phoneNumber"
-                      label="Phone no."
-                      variant="outlined"
-                      className="text-field"
-                    />
-                    <TextField
-                      required
-                      fullWidth
-                      value={order.address}
-                      onChange={handleChange}
-                      name="address"
-                      label="Address"
-                      variant="outlined"
-                      className="text-field"
-                    />
-                    <TextField
-                      required
-                      fullWidth
-                      value={order.email}
-                      onChange={handleChange}
-                      name="email"
-                      label="Email"
-                      variant="outlined"
-                      className="text-field"
-                      multiline
-                      maxRows={4}
-                    />
+      {loading ? (
+        <div className="cart">
+          <h1>Loading...</h1>
+          <div
+            className="cart-container"
+            style={{
+              alignItems: "center",
+            }}
+          >
+            <CircularProgress color="secondary" />
+          </div>
+        </div>
+      ) : (
+        <>
+          {cartItems.length ? (
+            <>
+              <div className="cart" style={{ marginBottom: 0 }}>
+                <h1>Cart Items</h1>
+                <div className="cart-container">
+                  <Button
+                    className="checkout-btn scroll-down"
+                    variant="contained"
+                    size="large"
+                    color="info"
+                    startIcon={<ExpandCircleDownIcon />}
+                    onClick={goToBottom}
+                  >
+                    Scroll Down
+                  </Button>
+
+                  {cartItems.map((item, itemIndex) => (
+                    <div className="cart-card" key={itemIndex}>
+                      <div className="cart-content">
+                        <Link to={`/products/${item._id}`}>
+                          <img src={item.image} alt={item.title} />
+                        </Link>
+                        <div className="cart-title">
+                          <Link to={`/products/${item._id}`}>
+                            <p>{item.title}</p>
+                          </Link>
+
+                          <p>
+                            Category:{" "}
+                            <Link
+                              to={`/products/${item.category.toLowerCase()}`}
+                            >
+                              {item.category}
+                            </Link>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="cart-btns">
+                        <div className="quantity-btns">
+                          <Button
+                            variant="text"
+                            color="warning"
+                            onClick={() => handleQuantity(item, -1)}
+                          >
+                            <RemoveCircleIcon />
+                          </Button>
+                          <p>
+                            Qty. {item.quantity} | Rs. {item.price}
+                          </p>
+                          <Button
+                            variant="text"
+                            color="success"
+                            onClick={() => handleQuantity(item, 1)}
+                          >
+                            <AddCircleIcon />
+                          </Button>
+                        </div>
+                        <Button
+                          className="remove-item"
+                          // variant="contained"
+                          color="error"
+                          onClick={() => removeFromCart(item.title)}
+                        >
+                          <RemoveShoppingCartIcon />
+                        </Button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="cart checkout" style={{ marginTop: 0 }}>
+                <h1>Place Order</h1>
+                <div className="cart-container">
+                  {checkOutTextField.map((data, index) => (
+                    <div className="cart-card checkout-card" key={index}>
+                      <TextField
+                        required
+                        value={data.value}
+                        onChange={handleChange}
+                        name={data.name}
+                        label={data.label}
+                        variant="filled"
+                        color="secondary"
+                      />
+                    </div>
+                  ))}
+                  {user ? (
                     <Button
-                      fullWidth
+                      className="checkout-btn"
                       onClick={handleSubmit}
                       variant="contained"
                       color="success"
-                      className="add-btn"
                       startIcon={<AddIcon />}
+                      size="large"
                     >
                       Order Submit
                     </Button>
-                    <Snackbar
-                      open={open}
-                      autoHideDuration={5000}
-                      onClose={handleClose}
-                      anchorOrigin={{
-                        vertical: "bottom",
-                        horizontal: "center",
-                      }}
+                  ) : (
+                    <Button
+                      className="checkout-btn"
+                      onClick={() => navigate("/login")}
+                      variant="contained"
+                      startIcon={<LoginIcon />}
+                      color="warning"
+                      size="large"
                     >
-                      <Alert
-                        onClose={handleClose}
-                        severity="error"
-                        sx={{ width: "100%" }}
-                      >
-                        Empty Field!
-                      </Alert>
-                    </Snackbar>
-                  </Container>
-                ) : (
-                  <>
-                    {" "}
-                    {cartItems.length ? (
-                      cartItems.map((item, itemIndex) => (
-                        <div className="cart-flex" key={itemIndex}>
-                          <div className="cart-container">
-                            <Link to={`/products/${item._id}`}>
-                              <div className="cart-content">
-                                <img
-                                  className="cart-image"
-                                  src={item.image}
-                                  alt={item.title}
-                                />
-                                <div className="cart-title">
-                                  <p>{item.title}</p>
-                                  <span className="saperater-min">|</span>
-                                  <p>
-                                    Qty. {(item.qty, 3)} | Rs. {item.price}
-                                  </p>
-                                </div>
-                              </div>
-                            </Link>
-                          </div>
-                        </div>
-                      ))
-                    ) : (
-                      <>
-                        {user ? (
-                          <>
-                            <Button
-                              onClick={() => navigate("/")}
-                              variant="contained"
-                              startIcon={<HomeIcon />}
-                              color="warning"
-                              size="large"
-                            >
-                              Back to home
-                            </Button>
-                          </>
-                        ) : (
-                          <>
-                            <Button
-                              onClick={() => navigate("/login")}
-                              variant="contained"
-                              startIcon={<LoginIcon />}
-                              color="warning"
-                              size="large"
-                            >
-                              Please Login
-                            </Button>
-                          </>
-                        )}
-                      </>
-                    )}
-                  </>
-                )}
-              </>
-            )}
-          </div>
-        </div>
-      </div>
+                      Please Login
+                    </Button>
+                  )}
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="cart">
+              <h1>Cart Items</h1>
+              <div className="cart-container">
+                <p>{message}</p>
+                <Button
+                  className="checkout-btn"
+                  onClick={() => navigate("/")}
+                  variant="contained"
+                  startIcon={<HomeIcon />}
+                  color="warning"
+                  size="large"
+                >
+                  Back to Home
+                </Button>
+              </div>
+            </div>
+          )}
+        </>
+      )}
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          Empty Fields!
+        </Alert>
+      </Snackbar>
     </>
   );
 };
