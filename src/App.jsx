dca26868e7ffcb98952da114dfcdcf8638c54181
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-10-04 03:52:46 +0500

    add: lazy, suspense loading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CartContainer } from "./Context/CartContext";
 import { ProductContainer } from "./Context/ProductContext";
@@ -7,14 +8,14 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Category from "./pages/Category";
 import CheckOut from "./pages/CheckOut";
-import Orders from "./pages/Orders";
-// import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AddProduct from "./pages/AddProduct";
-import EditProduct from "./pages/EditProduct";
+const Orders = lazy(() => import("./pages/Orders"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const AddProduct = lazy(() => import("./pages/AddProduct"));
+const EditProduct = lazy(() => import("./pages/EditProduct"));
 import Footer from "./components/Footer";
-import My404 from "./pages/My404";
+import My404 from "./components/My404";
+import { CircularProgress } from "@mui/material";
 
 export default function App() {
   const initialScrollTo = (value) => {
@@ -25,11 +26,17 @@ export default function App() {
   };
 
   return (
-    <>
-      <BrowserRouter>
-        <CartContainer>
-          <ProductContainer>
-            <Header />
+    <BrowserRouter>
+      <CartContainer>
+        <ProductContainer>
+          <Header />
+          <Suspense
+            fallback={
+              <div className="loading" style={{ marginTop: 300 }}>
+                <CircularProgress color="secondary" />
+              </div>
+            }
+          >
             <Routes>
               <Route path="*" exact={true} element={<My404 />} />
               <Route path="/" element={<Home />} />
@@ -46,10 +53,10 @@ export default function App() {
               />
               <Route path="/orders" element={<Orders />} />
             </Routes>
-            <Footer />
-          </ProductContainer>
-        </CartContainer>
-      </BrowserRouter>
-    </>
+          </Suspense>
+          <Footer />
+        </ProductContainer>
+      </CartContainer>
+    </BrowserRouter>
   );
 }
