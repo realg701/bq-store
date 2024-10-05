9ede1e8587ca16f797a92c168e92827283ba9ca0
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-10-06 04:59:22 +0500

    Update index.jsx

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -6,7 +6,7 @@ import { BE_URL } from "../../constants/url";
 import CartContext from "../../Context/CartContext";
 import ProductContext from "../../Context/ProductContext";
 import "./product.css";
-import EditIcon from "@mui/icons-material/Edit";
+import My404 from "../../components/My404";
 
 export default function Product() {
   const navigate = useNavigate();
@@ -23,7 +23,7 @@ export default function Product() {
   const [user, setUser] = React.useState(null);
   // Find Single Product From ProductContext
   const fetchSingleProduct = async () => {
-    if (!allProducts.length) return navigate("/");
+    // if (!allProducts.length) return navigate("/");
     setLoading(true);
     const findProduct = allProducts.find((product) => product._id === id);
     setSingleProduct(findProduct);
@@ -48,35 +48,31 @@ export default function Product() {
   React.useEffect(() => {
     fetchSingleProduct();
     const isUser = JSON.parse(localStorage.getItem("user"));
-    if (isUser) {
-      setUser(isUser);
-    }
+    if (isUser) setUser(isUser);
   }, []);
+
+  if (!singleProduct) return <My404 />;
 
   return (
     <>
-      <div className="container">
-        <div
-          className="header-image"
-          style={{ marginTop: 100, paddingInline: 30 }}
-        >
-          {/* <img
-            src="https://www.jdmedia.co.za/images/carousel/Ecommerce-Banner-1920.jpg"
-            alt="hero image"
-          /> */}
-          <Link to={"/"}>Home</Link>
-          {" / "}
-          <Link to={`/category/${singleProduct.category?.toLowerCase()}`}>
-            {singleProduct.category?.charAt(0).toUpperCase() +
-              singleProduct.category?.slice(1)}
-          </Link>{" "}
-          {" / "}
-          <Link>
-            {singleProduct.title?.charAt(0).toUpperCase() +
-              singleProduct.title?.slice(1)}
-          </Link>
-        </div>
+      <div
+        className="header-image"
+        style={{ marginTop: 100, paddingInline: 30 }}
+      >
+        <Link to={"/"}>Home</Link>
+        {" / "}
+        <Link to={`/category/${singleProduct.category?.toLowerCase()}`}>
+          {singleProduct.category?.charAt(0).toUpperCase() +
+            singleProduct.category?.slice(1)}
+        </Link>
+        {" / "}
+        <Link>
+          {singleProduct.title?.charAt(0).toUpperCase() +
+            singleProduct.title?.slice(1)}
+        </Link>
+      </div>
 
+      <div className="container">
         <div className="product-flex">
           {loading ? (
             <Material.CircularProgress color="secondary" />
