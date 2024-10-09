38e2f3172616fc70056367287342829a58fcabcd
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-10-10 04:13:29 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import "./App.css";
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
 import { CartContainer } from "./Context/CartContext";
 import { ProductContainer } from "./Context/ProductContext";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+const Category = lazy(() => import("./pages/Category"));
 import Product from "./pages/Product";
-import Category from "./pages/Category";
 import CheckOut from "./pages/CheckOut";
 const Orders = lazy(() => import("./pages/Orders"));
 const Login = lazy(() => import("./pages/Login"));
 const Register = lazy(() => import("./pages/Register"));
 const AddProduct = lazy(() => import("./pages/AddProduct"));
 const EditProduct = lazy(() => import("./pages/EditProduct"));
-import Footer from "./components/Footer";
 import My404 from "./components/My404";
-import { CircularProgress } from "@mui/material";
+import Footer from "./components/Footer";
+
+const fallback = (
+  <div className="loading" style={{ marginTop: 300 }}>
+    <CircularProgress color="secondary" />
+  </div>
+);
 
 export default function App() {
   return (
@@ -23,22 +29,16 @@ export default function App() {
       <CartContainer>
         <ProductContainer>
           <Header />
-          <Suspense
-            fallback={
-              <div className="loading" style={{ marginTop: 300 }}>
-                <CircularProgress color="secondary" />
-              </div>
-            }
-          >
+          <Suspense fallback={fallback}>
             <Routes>
-              <Route path=":my404" exact={true} element={<My404 />} />
               <Route path="/" element={<Home />} />
+              {/* 404 Page Route */}
+              <Route path="*" exact={true} element={<My404 />} />
               <Route path="/products/:id" element={<Product />} />
               <Route path="/category/:category" element={<Category />} />
-              {/* <Route path="/cart" element={<Cart />} /> */}
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/addproduct" element={<AddProduct />} />
+              <Route path="/products/addproduct" element={<AddProduct />} />
               <Route path="/products/edit/:id" element={<EditProduct />} />
               <Route path="/checkout" element={<CheckOut />} />
               <Route path="/orders" element={<Orders />} />
