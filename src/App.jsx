2753d801a2d5e5c6f950ad46b444a2ae42594d3c
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-10-11 09:50:26 +0500

    add: loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,56 @@
-import "./App.css";
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { CircularProgress } from "@mui/material";
+import { Box, LinearProgress, Typography } from "@mui/material";
 import { CartContainer } from "./Context/CartContext";
 import { ProductContainer } from "./Context/ProductContext";
+import { LoaderContainer } from "./Context/LoaderContext";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-const Category = lazy(() => import("./pages/Category"));
-import Product from "./pages/Product";
+const Categories = lazy(() => import("./pages/Categories"));
+const Category = lazy(() => import("./pages/Categories/Category"));
+const Product = lazy(() => import("./pages/Product"));
 import CheckOut from "./pages/CheckOut";
 const Orders = lazy(() => import("./pages/Orders"));
 const Login = lazy(() => import("./pages/Login"));
 const Register = lazy(() => import("./pages/Register"));
 const AddProduct = lazy(() => import("./pages/AddProduct"));
 const EditProduct = lazy(() => import("./pages/EditProduct"));
-import My404 from "./components/My404";
+const My404 = lazy(() => import("./components/My404"));
 import Footer from "./components/Footer";
-
-const fallback = (
-  <div className="loading" style={{ marginTop: 300 }}>
-    <CircularProgress color="secondary" />
-  </div>
-);
+import Loader from "./components/Loader";
+import NetworkStatusProvider from "./components/NetworkStatusProvider";
+import "./App.css";
 
 export default function App() {
+  const fallback = <Loader />;
+
   return (
-    <BrowserRouter>
-      <CartContainer>
-        <ProductContainer>
-          <Header />
-          <Suspense fallback={fallback}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              {/* 404 Page Route */}
-              <Route path="*" exact={true} element={<My404 />} />
-              <Route path="/products/:id" element={<Product />} />
-              <Route path="/category/:category" element={<Category />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/products/addproduct" element={<AddProduct />} />
-              <Route path="/products/edit/:id" element={<EditProduct />} />
-              <Route path="/checkout" element={<CheckOut />} />
-              <Route path="/orders" element={<Orders />} />
-            </Routes>
-          </Suspense>
-          <Footer />
-        </ProductContainer>
-      </CartContainer>
-    </BrowserRouter>
+    <NetworkStatusProvider>
+      <BrowserRouter>
+        <LoaderContainer>
+          <CartContainer>
+            <ProductContainer>
+              <Header />
+              <Suspense fallback={fallback}>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="*" exact element={<My404 />} />
+                  <Route path="/products/:id" element={<Product />} />
+                  <Route path="/categories" element={<Categories />} />
+                  <Route path="/categories/:category" element={<Category />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/products/addproduct" element={<AddProduct />} />
+                  <Route path="/products/edit/:id" element={<EditProduct />} />
+                  <Route path="/checkout" element={<CheckOut />} />
+                  <Route path="/orders" element={<Orders />} />
+                </Routes>
+              </Suspense>
+              <Footer />
+            </ProductContainer>
+          </CartContainer>
+        </LoaderContainer>
+      </BrowserRouter>
+    </NetworkStatusProvider>
   );
 }
