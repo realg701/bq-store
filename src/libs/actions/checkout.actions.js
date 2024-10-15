7f70b47e2c183430de734ac15a920c05885f43d2
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-10-15 18:07:04 +0500

    Update checkout.actions.js

--- src/libs/actions/checkout.actions.js
+++ src/libs/actions/checkout.actions.js
@@ -1,6 +1,59 @@
 import { initialScrollTo } from "../../constants";
 import { BE_URL } from "../../constants/url";
 
+export const checkOutTextFieldSet = (order) => {
+  return [
+    { value: order.name, name: "name", label: "Name" },
+    {
+      value: order.phoneNumber,
+      name: "phoneNumber",
+      label: "Phone no.",
+    },
+    {
+      value: order.address,
+      name: "address",
+      label: "Address",
+    },
+    { value: order.email, name: "email", label: "Email" },
+  ];
+};
+
+export const userDataSet = (token, order, total, cartItems) => {
+  return {
+    token: token,
+    name: order.name,
+    phoneNumber: order.phoneNumber,
+    address: order.address,
+    email: order.email,
+    total: total,
+    products: cartItems.map((item) => {
+      return {
+        _id: item._id,
+        title: item.title,
+        image: item.image,
+        price: item.price,
+        category: item.category,
+        seller: item.seller,
+        quantity: item.quantity,
+      };
+    }),
+  };
+};
+
+export const handleClose = (event, reason, setOpen) => {
+  if (reason === "clickaway") {
+    return;
+  }
+  setOpen(false);
+};
+
+export const handleChange = (e, order, setOrder) => {
+  const { value, name } = e.target;
+  setOrder(() => {
+    return { ...order, [name]: value };
+  });
+};
+
 export const handleSubmit = async (
   userData,
   setOpen,
@@ -35,6 +88,10 @@ export const handleSubmit = async (
       if (data.status === "success") {
         setCartItems([]);
         setMessage("Order Placed Successfully");
+        localStorage.setItem(
+          "userInput",
+          JSON.stringify({ name, phoneNumber, address, email })
+        );
       }
       if (data.status === "empty") {
         setMessage("Your Cart Is Empty");
