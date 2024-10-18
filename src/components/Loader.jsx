37eed98e137ab7821168d41e46e7fc7562d5502d
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-10-18 19:52:18 +0500

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,18 +1,13 @@
 import { Box, CircularProgress, LinearProgress } from "@mui/material";
 
-export default function Loader({ loading, ring, top }) {
+export default function Loader({ ring, top }) {
   const style = {
     zIndex: 3,
   };
 
   return (
     <Box position={"relative"}>
-      <LinearProgress
-        color="secondary"
-        sx={style}
-        variant={loading ? "determinate" : "indeterminate"}
-        value={100}
-      />
+      <LinearProgress color="secondary" sx={style} value={100} />
       {ring && (
         <Box
           sx={{
