90282e1a9221b9d3f297aed87c410362803363e9
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-10-18 19:52:12 +0500

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,10 @@
-import { useContext } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import CartContext from "../../Context/CartContext";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Login } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import Drawer from "../Drawer";
 import Dashboard from "../Dashboard";
-import { Badge, IconButton } from "@mui/material";
-import { Login, ShoppingCart } from "@mui/icons-material";
-import "./Header.css";
 import CartDrawer from "../CartDrawer";
+import "./Header.css";
 
 const withouSidebarRoutes = ["<routes>"];
 
@@ -14,8 +12,6 @@ export default function Header() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const user = JSON.parse(localStorage.getItem("user"));
-  // const cartContext = useContext(CartContext);
-  // const { cartItems } = cartContext;
   // Validates if the current pathname includes one the routes you want to hide the sidebar is present on the current url
   // If that's true render null instead of the sidebar
   if (withouSidebarRoutes.some((item) => pathname.includes(item))) return null;
@@ -38,13 +34,6 @@ export default function Header() {
           ) : (
             <Dashboard />
           )}
-          {/* <Link to={"/checkout"}>
-            <IconButton>
-              <Badge badgeContent={String(cartItems.length)} color="secondary">
-                <ShoppingCart fontSize="large" style={{ color: "#ffff90" }} />
-              </Badge>
-            </IconButton>
-          </Link> */}
           <CartDrawer />
         </div>
       </div>
