49961d2d1e5d5cef96b973af30d660e44d80e24c
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-10-22 19:58:12 +0500

    Update index.jsx

--- src/components/TextEditor/index.jsx
+++ src/components/TextEditor/index.jsx
@@ -1,61 +1,58 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
-import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-import ExampleTheme from "./ExampleTheme";
-import ToolbarPlugin from "./plugins/ToolbarPlugin";
-import TreeViewPlugin from "./plugins/TreeViewPlugin";
-import "./style.css";
-import { useState } from "react";
-const placeholder = "Enter some rich text...";
+export default function TextEditor({ user, setUser }) {
+  const [value, setValue] = useState("");
 
-const editorConfig = {
-  namespace: "React.js Demo",
-  nodes: [],
-  // Handling of errors during update
-  onError(error) {
-    throw error;
-  },
-  // The editor theme
-  theme: ExampleTheme,
-};
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
 
-export default function TextEditor() {
-  const [state, setState] = useState("");
-  console.log(state);
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ];
+
+  useEffect(() => {
+    setUser({ ...user, bio: value });
+  }, [value]);
 
   return (
-    <LexicalComposer initialConfig={editorConfig}>
-      <div className="editor-container">
-        <ToolbarPlugin />
-        <div className="editor-inner">
-          <RichTextPlugin
-            contentEditable={
-              <ContentEditable
-                className="editor-input"
-                aria-placeholder={placeholder}
-                placeholder={
-                  <div className="editor-placeholder">{placeholder}</div>
-                }
-              />
-            }
-            ErrorBoundary={LexicalErrorBoundary}
-          />
-          <HistoryPlugin />
-          <AutoFocusPlugin />
-          {/* <TreeViewPlugin /> */}
-        </div>
-      </div>
-    </LexicalComposer>
+    <ReactQuill
+      formats={formats}
+      modules={modules}
+      theme="snow"
+      value={value}
+      onChange={setValue}
+    />
   );
 }
